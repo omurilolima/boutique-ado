2cb8a47a3e7cc01a191ee1f06ddae46624587fe8
omurilolima <>
2023-05-15 16:19:26 +0000

    Add category filtering

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
