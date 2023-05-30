b3b14d030535b1b1e8eac9b6daea4899413f115f
omurilolima <>
2023-05-30 15:10:34 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
 )
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -14,6 +16,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -35,6 +38,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
 
@@ -68,7 +72,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
+    """Remove the item from the shopping bag"""'
 
     try:
         size = None
