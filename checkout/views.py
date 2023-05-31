9b1ada0a52f68d19531acd1f94b8edf74873694e
omurilolima <>
2023-05-31 20:29:18 +0000

    Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NDuTjLsB8Pejk6gQN3hr3C16UxtN2HXsutuSM7A7kb80UVfSLnxAAbYxiUWqZ9Y8D8dBk0pdXa95x3lClrbhefR00lzF1okmn',  # noqa
+        'client_secret': 'pk_test_51NDuTjLsB8Pejk6gQN3hr3C16UxtN2HXsutuSM7A7kb80UVfSLnxAAbYxiUWqZ9Y8D8dBk0pdXa95x3lClrbhefR00lzF1okmn',  # noqa
     }
 
     return render(request, template, context)
