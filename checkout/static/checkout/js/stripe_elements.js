10d683c125c8b7bb42db2ce54514df4043e3083c
omurilolima <>
2023-06-06 19:17:50 +0000

    Add loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -52,6 +52,8 @@ form.addEventListener('submit', function(ev){
     ev.preventDefault();
     card.update({'disable': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -65,6 +67,8 @@ form.addEventListener('submit', function(ev){
             </span>
             <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disable': false });
             $('#submit-button').attr('disabled', false);
         } else {
