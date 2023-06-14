4a2e1671fbea554021134e30f528c3660329e4c7
omurilolima <>
2023-06-14 15:25:37 +0000

    Update AWS config again

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,7 +178,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGES_BUCKET_NAME = 'murilo-boutiqueado'
+    AWS_STORAGE_BUCKET_NAME = 'murilo-boutiqueado'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
